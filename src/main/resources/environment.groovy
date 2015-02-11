ce205e18bb56eaeda1a4b29c2244b3a1876b1b18
trungquangphan <trungquang.phan@axonactive.vn>
2015-02-11 14:54:33 +0700

    'SimpleConfiguration'

---
+++ src/main/resources/environment.groovy
@@ -0,0 +1,14 @@
+environments {
+    dev {
+        host = 'localhost'
+        webapp {
+            port = 8080
+        }
+    }
+    prod {
+        host = 'www.chrislormor.com'
+        webapp {
+            port = 8081
+        }
+    }
+}
